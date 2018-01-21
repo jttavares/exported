e5cb4b13821a660db379fec10718bada5206197d
jttavares <32249679+jttavares@users.noreply.github.com>
2018-01-20 22:52:48 -0200

    AirBnBLandingPage

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+btn.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+} 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById('navbarMenu');
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
